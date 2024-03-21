f94f343b1e11759075ab8d1a115b12a5aa40db03
allan <allanrey.macasaquit@gmail.com>
2024-03-20 23:14:27 -0400

    Create Contentful cms

--- cms/src/App.jsx
+++ cms/src/App.jsx
@@ -1,6 +1,8 @@
 import Hero from './components/Hero';
-
+import useFetchProjects from './contentful_cms';
 function App() {
+	const { loading, projects } = useFetchProjects();
+	console.log(loading, projects);
 	return (
 		<main>
 			<Hero />
