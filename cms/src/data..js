7a9aaffc971501004e5d2d4cc0f8729f8b5cf7a5
allan <allanrey.macasaquit@gmail.com>
2024-03-20 23:24:29 -0400

    Tourism added

--- cms/src/data..js
+++ cms/src/data..js
@@ -1,23 +1,13 @@
 const projects = [
 	{
-		title: 'birthday buddy',
-		url: 'https://react-vite-projects-1-birthday-buddy.netlify.app/',
-		image: './assets/images/birthday.png',
-	},
-	{
-		title: 'tours',
-		url: 'https://react-vite-projects-2-tours.netlify.app/',
-		image: './assets/images/tours.png',
-	},
-	{
 		title: 'Store',
 		url: 'https://store-react-app.netlify.app/',
 		image: './assets/images/store.png',
 	},
 	{
-		title: 'questions',
-		url: 'https://react-vite-projects-4-accordion.netlify.app/',
-		image: './assets/images/questions.png',
+		title: 'tourism',
+		url: 'https://tourism-react18.netlify.app/',
+		image: './assets/images/tourism.png',
 	},
 ];
 
