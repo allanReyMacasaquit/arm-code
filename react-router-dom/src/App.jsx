fa6c17650563c34a734e8b74cdcf27a9267cbf29
allan <allanrey.macasaquit@gmail.com>
2024-03-21 09:42:06 -0400

    Basic Router

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,9 +1,21 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <h2>home page</h2>,
+	},
+	{
+		path: '/about',
+		element: (
+			<div>
+				<h2>about page</h2>
+			</div>
+		),
+	},
+]);
+
 function App() {
-	return (
-		<main>
-			<h1>React Router Dom Project</h1>
-		</main>
-	);
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
