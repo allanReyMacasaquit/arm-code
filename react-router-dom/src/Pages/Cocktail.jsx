23ab7fb8c8092e2e282b00743880b7ceb9656cba
allan <allanrey.macasaquit@gmail.com>
2024-03-23 14:33:41 -0400

    add valid ingredients

--- react-router-dom/src/Pages/Cocktail.jsx
+++ react-router-dom/src/Pages/Cocktail.jsx
@@ -1,10 +1,73 @@
 import axios from 'axios';
 import Wrapper from '../wrappers/CocktailPage';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Cocktail = () => {
 	const { id, data } = useLoaderData();
-	return <Wrapper>Cocktail</Wrapper>;
+	const singleDrink = data.drinks[0];
+
+	const {
+		strDrink: name,
+		strAlcoholic: info,
+		strCategory: category,
+		strDrinkThumb: image,
+		strGlass: glass,
+		strInstructions: instructions,
+	} = singleDrink;
+
+	const validIngredient = Object.keys(singleDrink)
+		.filter(
+			(key) => key.startsWith('strIngredient') && singleDrink[key] !== null
+		)
+		.map((key) => singleDrink[key]);
+
+	return (
+		<Wrapper>
+			<header>
+				<Link to='/' className='btn'>
+					back to home
+				</Link>
+			</header>
+			<section className='drink-container'>
+				<div className='drink'>
+					<img src={image} className='img' alt={name} />
+					<div className='drink-info'>
+						<p className='drink-data'>
+							<span>name: </span>
+							{name}
+						</p>
+						<p className='drink-data'>
+							<span>category: </span>
+							{category}
+						</p>
+						<p className='drink-data'>
+							<span>info: </span>
+							{info}
+						</p>
+						<p className='drink-data'>
+							<span>glass: </span>
+							{glass}
+						</p>
+						<p className='drink-data'>
+							<span>Ingredients: </span>
+							{validIngredient.map((item, index) => {
+								return (
+									<span className='ing' key={item}>
+										{item}
+										{index < validIngredient.length - 1 ? ',' : ''}
+									</span>
+								);
+							})}
+						</p>
+						<p className='drink-data'>
+							<span>instructions: </span>
+							{instructions}
+						</p>
+					</div>
+				</div>
+			</section>
+		</Wrapper>
+	);
 };
 export default Cocktail;
 
--- react-router-dom/src/wrappers/CocktailPage.js
+++ react-router-dom/src/wrappers/CocktailPage.js
@@ -1,48 +1,52 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  header {
-    text-align: center;
-    margin-bottom: 3rem;
-    .btn {
-      margin-bottom: 1rem;
-    }
-  }
-  .img {
-    border-radius: var(--borderRadius);
-  }
-  .drink-info {
-    padding-top: 2rem;
-  }
-  .drink p {
-    font-weight: 700;
-    text-transform: capitalize;
-    line-height: 2;
-    margin-bottom: 1rem;
-  }
-  .drink-data {
-    margin-right: 0.5rem;
-    background: var(--primary-300);
-    padding: 0.25rem 0.5rem;
-    border-radius: var(--borderRadius);
-    color: var(--primary-700);
-    letter-spacing: var(--letterSpacing);
-  }
-  .ing {
-    display: inline-block;
-    margin-right: 0.5rem;
-  }
-  @media (min-width: 992px) {
-    .drink {
-      display: grid;
-      grid-template-columns: 2fr 3fr;
-      gap: 3rem;
-      align-items: center;
-    }
-    .drink-info {
-      padding-top: 0;
-    }
-  }
+	header {
+		text-align: center;
+		margin-bottom: 3rem;
+		.btn {
+			margin-bottom: 1rem;
+		}
+	}
+	.img {
+		box-shadow: var(--shadow-2);
+		padding: 1px;
+		border-radius: 20px;
+	}
+	.drink-info {
+		background-color: var(--primary-100);
+		border-radius: 20px;
+		padding: 2px 0;
+		margin: 4px 0;
+	}
+	.drink p {
+		font-weight: 700;
+		text-transform: capitalize;
+		line-height: 2;
+		margin: 1rem;
+	}
+	.drink-data {
+		margin-right: 0.5rem;
+		background: var(--primary-300);
+		padding: 0.25rem 0.5rem;
+		border-radius: var(--borderRadius);
+		color: var(--primary-700);
+		letter-spacing: var(--letterSpacing);
+	}
+	.ing {
+		display: inline-block;
+		margin-right: 0.5rem;
+	}
+	@media (min-width: 992px) {
+		.drink {
+			display: grid;
+			grid-template-columns: 2fr 3fr;
+			gap: 3rem;
+			align-items: center;
+		}
+		.drink-info {
+		}
+	}
 `;
 
 export default Wrapper;
